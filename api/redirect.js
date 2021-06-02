c06215b62725ac2108a7ba4b5ed33eb3f1606d14
Kam <seekayel@users.noreply.github.com>
2021-06-02 06:07:27 -0400

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,4 +1,6 @@
 module.exports.all = (req,res) => {
   res.status(302)
-  res.set('Location','http://localhost') 
+  res.set('Location','http://localhost')
+  res.set('Content-Type','text/plain')
+  res.body('')
 }
