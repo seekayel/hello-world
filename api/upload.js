43e697ba2305a8060317b871d483880419759f29
Kam Lasater <ckl@seekayel.com>
2021-06-03 16:24:35 -0400

    Added scoped generateURL call

--- api/upload.js
+++ api/upload.js
@@ -2,7 +2,7 @@ const { S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 
 
-module.exports.generateURL = async (contentType) => {
+exports.generateURL = async (contentType) => {
   // bucket='files-829947040538'
   bucket='cyclic-sam-artifacts-542703605194'
   key=`my/awesome/path/README-${Date.now()}.md`
@@ -17,8 +17,8 @@ module.exports.generateURL = async (contentType) => {
   return url
 }
 
-module.exports.put = (req,res) => {
-  const url = generateURL(req.get('content-type'))
+exports.put = (req,res) => {
+  const url = exports.generateURL(req.get('content-type'))
   res.status = 307
   res.set('Location',url)
   res.json({url})
