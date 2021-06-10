f04d6fc563789e01ab73012bc03bdbdac6bf703b
Kam <seekayel@users.noreply.github.com>
2021-06-10 11:28:10 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@ module.exports.all = (req,res) => {
   res.json({
     Hello: ['Hola', 'こんにちは', 'Hello', 'Привет', 'Olá', '你好', 'Hi'],
     World: '🌎',
-    at: Date.now()
+    at: new Date().toISOString()
   }
 )}
