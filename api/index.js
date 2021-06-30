95ef2d01ae4f1442d4235a9a31d901ed276d9528
Kam <seekayel@users.noreply.github.com>
2021-06-30 07:20:56 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ module.exports.all = (req,res) => {
   res.json({
     Hello: ['Hola', 'こんにちは', 'Hello', 'Привет', 'Olá', '你好', 'Hi'],
     World: ['🌎','🌍','🌏'],
-    at: new Date().toISOString()
+    at: new Date().toISOString(),
+    msg: "Every day is a chance to improve."
   }
 )}
