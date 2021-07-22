32ed16ed434887aa7296bc15f1a1d702ab09fe58
Kam <seekayel@users.noreply.github.com>
2021-07-21 20:04:36 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 module.exports.all = (req,res) => {
   res.json({
-    Hello: ['Hola', 'こんにちは', 'Hello', 'Привет', 'Olá', '你好', 'Hi'],
+    Hello: ['Hola', 'こんにちは', 'Hello', 'Привет', 'Olá', '你好', 'Hi','Howdy'],
     World: ['🌎','🌍','🌏'],
     at: new Date().toISOString(),
     msg: "You are off to great places."
