bd90aed0c1868afb904c7843c57faf2377db6e42
Kam <seekayel@users.noreply.github.com>
2021-07-28 07:17:56 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 module.exports.all = (req,res) => {
+  console.log(`req: ${JSON.stringify(req,null,2)}`)
   res.json({
     Hello: ['Hola', 'こんにちは', 'Hello', 'Привет', 'Olá', '你好', 'Hi','Howdy'],
     World: ['🌎','🌍','🌏'],
